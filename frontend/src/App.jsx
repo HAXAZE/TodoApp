34521d3fe770391023f2ddc812f8064565b2ccf7
HAXAZE <28skofficial01@gmail.com>
2024-01-14 16:05:10 +0530

    connected frontend with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,15 @@ import { Todos } from './components/Todos'
 
 function App() {
 
+  const [todos,setTodos]=useState([]);
+  //  ugly way of connecting with backend 
+  fetch("http://localhost:3000/todos")
+    .then(async function(res){
+      const json = await res.json();
+      setTodos(json.todos);
+    })
   return (
-
+ 
     <div>
         <CreateTodo/>
         <Todos/>
