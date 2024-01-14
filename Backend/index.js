622b21c4135e35650d30f2f59ac49d840a4fa879
HAXAZE <28skofficial01@gmail.com>
2024-01-14 12:54:13 +0530

    coded the part of todo

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,56 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
+const express = require("express");
+const { CreateTodo } = require("./zod");
 
-mongoose.connect(process.env.MONGODB_URL);
+const app = express();
 
-const TodoSchema = mongoose.Schema({
-    title: String,
-    description: String,
-    completed: Boolean
-});
+app.use(express.json());
 
-const todo = mongoose.model('TodoApp', TodoSchema);
 
-module.exports = {
-    todo
-};
+app.post("/todo", async function (req, res) {
+    const createPayload = req.body;
+    const parsePayload = CreateTodo.safeParse(createPayload);
+    if (!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed:false
+    })
+    res.json({
+        msg: "Todo Created"
+    })
+
+})
+
+app.get("/todos", async function (req, res) {
+    const todos = await todo.find({});
+
+    res.json({
+        todos
+    })
+})
+
+app.put("/completed", async function (req, res) {
+    const updatePayLoad = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayLoad);
+    if (!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed:true
+    })
+    res.json({
+        msg:"Todo marked as completed"
+    })
+})
+
+app.listen(3000);
