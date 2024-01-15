5034e4ff45a3b6fb3a7fd4c8ca3712e57a49e773
HAXAZE <28skofficial01@gmail.com>
2024-01-15 12:22:21 +0530

    completed the todo app & connected with backend

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,46 +1,46 @@
 import { useState } from "react";
 
-export function CreateTodo(){
+export function CreateTodo(props) {
+    // react-query
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
 
-    const [title,setTitle] = useState("");
-    const [description,setDescription]= useState("");
     return <div>
-
         <input id="title" style={{
-            padding:10,
-            margin:10
-        }} type="text" placeholder="title" onChange={function(e){
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="title" onChange={function(e) {
             const value = e.target.value;
             setTitle(e.target.value);
-        }}></input><br/>
+        }}></input> <br />
+    
         <input id="desc" style={{
-            padding:10,
-            margin:10
-        }} type="text" placeholder="description" onChange={function(e){
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="description" onChange={function(e) {
             const value = e.target.value;
             setDescription(e.target.value);
-        }}></input> <br/>
+        }}></input> <br />
 
         <button style={{
-            padding:10,
-            margin:10
-            // connected the backend with fetch
-        }}onClick={()=>{
-            fetch("http://localhost:3000/todo",{
-                method:"POST",
-                body:JSON.stringify({
+            padding: 10,
+            margin: 10
+        }} onClick={() => {
+            // axios
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
                     title: title,
-                    description:description
+                    description: description
+                }),
+                headers: {
+                    "Content-type": "application/json"
                 }
-            ),
-            headers:{
-                "Content-type":"application/json"
-            }
-        })
-            .then(async function(res){
-                const json = await res.json();
-                alert("Todo Added");
             })
-        }}>Add a Todo</button>
+                .then(async function(res) {
+                    const json = await res.json();
+                    alert("Todo added");
+                })
+        }}>Add a todo</button>
     </div>
 }
